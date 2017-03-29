b65891328bbbff435f1611fbc27e393d1e0d2734
Jim Zhu <flappyboby@gmail.com>
2017-03-29 00:43:11 -0400

    Initial commit

---
+++ src/kanban/KanbanController.java
@@ -0,0 +1,12 @@
+package kanban;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class KanbanController {
+    @FXML private Text actiontarget;
+    @FXML protected void handleSubmitButtonAction(ActionEvent event){
+        actiontarget.setText("Sign in button pressed.");
+    }
+}
