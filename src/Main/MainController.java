dc2a88f296b688610c8abc28fbd6bc66824314a6
Jim Zhu <flappyboby@gmail.com>
2017-04-02 02:03:09 -0400

    Added new task feature.

--- src/Main/MainController.java
+++ src/Main/MainController.java
@@ -2,11 +2,27 @@ package Main;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 public class MainController {
-    @FXML private Text actiontarget;
-    @FXML protected void handleSubmitButtonAction(ActionEvent event){
-        actiontarget.setText("Sign in button pressed.");
+    @FXML
+    private Text actiontarget;
+    @FXML
+    private FlowPane backlogLane;
+
+    @FXML
+    public void initialize() {
     }
+
+    @FXML
+    protected void handleCreateNewTask(ActionEvent event) {
+        actiontarget.setText("New Task Created!");
+        VBox test = new VBox();
+        test.setStyle("-fx-border-color: black");
+        test.getChildren().add(new Text("Hi"));
+        backlogLane.getChildren().add(test);
+    }
+
 }
